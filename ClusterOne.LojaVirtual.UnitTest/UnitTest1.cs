4f4c14d7ffdf776d12ac595095cc2ce4ea94d3ac
Matheus Augusto Proença Correa <hardymath@hotmail.com>
2015-12-16 13:56:32 -0200

    Initial Commit

---
+++ ClusterOne.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClusterOne.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class ClusterOneTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ClusterOne.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClusterOne.LojaVirtual.Web.MvcApplication" Language="C#" %>
